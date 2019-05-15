a1d055b64dd9c0269860d51c543cc5fa6bf07faf
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-05-16 01:52:18 +0300

    Tüm postları listeleyen Get metodu eklendi.

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("This is posts page.");
+router.get("/", async (req, res) => {
+  try {
+      const posts = await Post.find();
+      res.json(posts);
+  } catch (err) {
+      res.json({message: err});
+  }
 });
 
 router.post("/", async (req, res) => {
