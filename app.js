0424b635b767ce39b59f7eeadfd69f99146abde8
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-05-16 01:19:58 +0300

    Veritabanı bağlantısı yapıldı. Basit bir routing ve middleware yapısı oluşturuldu.

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
 
-// ROUTES
+//For reading .env file;
+require('dotenv').config()
+
+//Import routes;
+const postsRoute = require('./routes/posts');
+
+//Middlewares;
+app.use('/posts', postsRoute);
+
+//Routing;
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+//Connecting to database;
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => {
+    console.log("Connected to database!");
+  }
+);
+
+//Start listening to server;
+app.listen(3000);
