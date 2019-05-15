cbef20821b2eea65719114c4be41e5b5a706e102
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-05-16 02:01:32 +0300

    ID sine post getirme yapıldı.

--- routes/posts.js
+++ routes/posts.js
@@ -2,29 +2,43 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+// GET ALL POSTS
 router.get("/", async (req, res) => {
-  try {
-      const posts = await Post.find();
-      res.json(posts);
-  } catch (err) {
-      res.json({message: err});
-  }
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
 });
 
-router.post("/", async (req, res) => {
-  const post = new Post({
-    title: req.body.title,
-    description: req.body.description
-  });
+// GET POST BY ID
+router.get("/:postId", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json(err);
+    }
+});
 
-  try{
-    const savedPost = await post.save();
-    res.json(savedPost);
-  }
-  catch(err){
-    res.json({message: err});
-  }
-    
+// SUBMITS A POST
+router.post("/", async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    try {
+        const savedPost = await post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({
+            message: err
+        });
+    }
 });
 
 module.exports = router;
