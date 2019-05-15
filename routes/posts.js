8a316c3aa78a9cd560fc10dc280eae973c72db5e
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-05-16 02:05:40 +0300

    Delete metodu eklendi.

--- routes/posts.js
+++ routes/posts.js
@@ -4,41 +4,51 @@ const Post = require("../models/Post");
 
 // GET ALL POSTS
 router.get("/", async (req, res) => {
-    try {
-        const posts = await Post.find();
-        res.json(posts);
-    } catch (err) {
-        res.json({
-            message: err
-        });
-    }
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({
+      message: err
+    });
+  }
 });
 
 // GET POST BY ID
 router.get("/:postId", async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.postId);
-        res.json(post);
-    } catch (err) {
-        res.json(err);
-    }
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // SUBMITS A POST
 router.post("/", async (req, res) => {
-    const post = new Post({
-        title: req.body.title,
-        description: req.body.description
-    });
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
 
-    try {
-        const savedPost = await post.save();
-        res.json(savedPost);
-    } catch (err) {
-        res.json({
-            message: err
-        });
-    }
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({
+      message: err
+    });
+  }
 });
+
+// DELETE A POST
+router.delete("/:postId", async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({message: err});
+    }
+  });
 
 module.exports = router;
